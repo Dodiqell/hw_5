8e031d3d70de82a784aa4b73b4a2bf9de5c8f71f
Dodiqell <petrovskih2015@yandex.ru>
2023-06-13 21:55:25 +0500

    hw_5 проверьте дз

--- src/Main.java
+++ src/Main.java
@@ -30,6 +30,23 @@ public class Main {
         } else {
                     System.out.println("Не високосный год");
                 }
+        //Задача 4
+
+        int deliveryDistance = 95;
+        if (deliveryDistance <= 20){
+            System.out.println("Потребуется дней для доставки: 1");
+        }
+        else if (deliveryDistance > 20 && deliveryDistance < 60){
+            System.out.println("Потребуется дней для доставки: 2");
+        }
+        else if (deliveryDistance > 60 && deliveryDistance <= 100){
+
+        }
+        System.out.println("Потребуется дней для доставки: 3");
+             if (deliveryDistance > 100) {
+                System.out.println("Доставки нет");
+            }
+
         //Задача 5
         int month = 13;
         switch (month){
